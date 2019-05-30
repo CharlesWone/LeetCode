56ba503f896b11fd4f5910611cb1173737d5b2ad
CharlesWone <925552075@qq.com>
2019-05-30 19:27:10 +0800

    66. 加一

--- LeetCode/PlusOne_66.java
+++ LeetCode/PlusOne_66.java
@@ -19,14 +19,17 @@
 public class PlusOne_66 {
     public int[] plusOne(int[] digits) {
         digits[digits.length - 1]++;
-        for (int i = digits.length - 1; i >= 0; i--) {
+        for (int i = digits.length - 1; i > 0; i--) {
             if (digits[i] == 10) {
                 digits[i] = 0;
                 digits[i - 1]++;
+            } else {
+                return digits;
             }
         }
 
         if (digits[0] == 10) {
+            digits[0] = 0;
             int[] newArr = new int[digits.length + 1];
             newArr[0] = 1;
             System.arraycopy(digits, 0, newArr, 1, digits.length);
