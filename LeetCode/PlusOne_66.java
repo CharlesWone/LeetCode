9fe9510e9fb1c4b450aee0170399eb2578238df6
FacelessMen <925552075@qq.com>
2019-05-30 18:07:06 +0800

    66. 加一

---
+++ LeetCode/PlusOne_66.java
@@ -0,0 +1,37 @@
+/*
+给定一个由整数组成的非空数组所表示的非负整数，在该数的基础上加一。
+
+最高位数字存放在数组的首位， 数组中每个元素只存储一个数字。
+
+你可以假设除了整数 0 之外，这个整数不会以零开头。
+
+示例 1:
+
+输入: [1,2,3]
+输出: [1,2,4]
+解释: 输入数组表示数字 123。
+示例 2:
+
+输入: [4,3,2,1]
+输出: [4,3,2,2]
+解释: 输入数组表示数字 4321。
+ */
+public class PlusOne_66 {
+	public int[] plusOne(int[] digits) {
+		digits[digits.length - 1]++;
+		for (int i = digits.length - 1; i >= 0; i--) {
+			if (digits[i] >= 10) {
+				digits[i] %= 10;
+				if (i > 1) {
+					digits[i - 1]++;
+				} else {
+					int[] newArr = new int[digits.length + 1];
+					newArr[0] = 1;
+					System.arraycopy(digits, 0, newArr, 1, digits.length);
+					return newArr;
+				}
+			}
+		}
+		return digits;
+	}
+}
